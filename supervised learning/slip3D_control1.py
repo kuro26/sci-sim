647ceb2e98034681019da2a82f60e74e87b836fa
圣君 王 <stevenking26@outlook.com>
2018-03-27 16:11:48 +0800

    简化了雅可比矩阵的计算，其实就是合并了两个循环

--- supervised learning/slip3D_control1.py
+++ supervised learning/slip3D_control1.py
@@ -7,11 +7,38 @@ import numpy as np
 
 
 # ------------------------------------------------
-#              在简化模型层给出控制参数
+#              获取控制线性梯度表格
 # des_vel，期望速度,仅选取表中的速度
 # x_now， 当前的状态
 # 状态： [h0, vx0, vy0]
 # ------------------------------------------------
+def get_next_apex_status(pair):
+    sol1, sol2, sol3, sol4, foot_point = slip3D_ex.sim_cycle(pair)
+    return sol4.y[:, -1][2:5]
+
+
+def conrol_jac_calculation(pair):
+    m_apex = get_next_apex_status(pair)
+    # jac x的计算
+    jac_combine = np.zeros(shape=(3, 7))
+    for col in range(7):
+        dm_pair = np.array(pair)
+        if dm_pair[col] < 10:              # 数值计算delta的选取
+            dx = 0.001
+        else:
+            dx = dm_pair[col] * 0.001
+        dm_pair[col] = dm_pair[col] + dx
+        dm_apex = get_next_apex_status(dm_pair)
+        res = (dm_apex - m_apex) / dx
+        jac_combine[:, col] = np.array(res).transpose()
+    jac_x = jac_combine[:, 0:3]
+    jac_u = jac_combine[:, 3:8]
+
+
+def control_table_establish():
+    m_table = pd.read_csv('./data/stable_pair.csv', header=None).values
+    vel_list = m_table[:, 1]
+
 def control_para_calculation(des_vel, x_now):
     # 1.检查给定的期望速度值是否在范围内
     m_table = pd.read_csv('./data/stable_pair.csv', header=None).values
--- supervised learning/test_script.py
+++ supervised learning/test_script.py
@@ -13,37 +13,27 @@ m_table = pd.read_csv('./data/stable_pair.csv', header=None).values
 vel_list = m_table[:, 1]
 m_idx = np.fabs(vel_list - 4.25).argmin()
 m_pair = m_table[m_idx]
-# Jacobian x的计算
-jac_x = np.zeros(shape=(3, 3))
-for col in range(3):
-    dm_pair = np.array(m_pair)
-    if dm_pair[col] < 10:
-        dx = 0.001
-    else:
-        dx = dm_pair[col] * 0.001
-    dm_pair[col] = dm_pair[col] + dx
-    m_apex = get_next_apex_status(m_pair)
-    dm_apex = get_next_apex_status(dm_pair)
-    res = (dm_apex - m_apex) / dx
-    print('col:', col)
-    print(res)
-    jac_x[:, col] = np.array(res).transpose()
-    print(jac_x)
 
-# Jacobian u的计算
-jac_u = np.zeros(shape=(3, 4))
-col_list = [3, 4, 5, 6]
-for col in col_list:
+m_apex = get_next_apex_status(m_pair)
+jac_combine = np.zeros(shape=(3, 7))
+for col in range(7):
     dm_pair = np.array(m_pair)
-    if dm_pair[col] < 10:
+    if dm_pair[col] < 10:  # 数值计算delta的选取
         dx = 0.001
     else:
         dx = dm_pair[col] * 0.001
     dm_pair[col] = dm_pair[col] + dx
-    m_apex = get_next_apex_status(m_pair)
     dm_apex = get_next_apex_status(dm_pair)
     res = (dm_apex - m_apex) / dx
-    print('col:', col)
-    print(res)
-    jac_u[:, col-3] = np.array(res).transpose()
-    print(jac_u)
+    jac_combine[:, col] = np.array(res).transpose()
+jac_x = jac_combine[:, 0:3]
+jac_u = jac_combine[:, 3:8]
+
+jac_u_2 = jac_u[:, 0:3]
+print(jac_u_2)
+jac_u_2[:, 2] = jac_u_2[:, 2] - jac_u[:, -1]
+print(jac_u_2)
+
+j_total = np.dot(np.linalg.inv(jac_u_2), jac_x)
+print(j_total)
+
