b04a80bf419dcab2a6ebe49a5553817c224c04f6
圣君 王 <stevenking26@outlook.com>
2018-04-24 22:57:39 +0800

    开始控制

--- supervised learning/biped_sim.py
+++ supervised learning/biped_sim.py
@@ -5,6 +5,9 @@ import pandas as pd
 import numpy as np
 from . import slip3D_ex
 
+g = 9.8
+sim_cycle = 0.01
+
 
 # 奔跑控制总程序
 class BipedController:
@@ -14,11 +17,16 @@ class BipedController:
         self.robot_id = robot_id
         self.dic_vel_jac = {}         # 由速度索引的控制雅可比矩阵
         self.dic_vel_time = {}        # 速度索引半周期
-        self.pair_table = []
+        self.pair_table = np.array([])
+        # 本周期相关变量
+        self.this_x = []              # 起始顶点状态
+        self.this_rv = 0              # 参考速度
+        self.this_pair = []           # 控制对
+        self.this_du = []             # 无系数du
 
-    # 导入表格，生成控制雅可比矩阵
+    # ----------导入表格，生成控制雅可比矩阵----------
     def load_table(self, pair_path):
-        pair_table = pd.read_csv(pair_path)
+        pair_table = pd.read_csv(pair_path, header=None).values
         table_len = pair_table.shape[0]
         for idx in range(table_len):
             pair = pair_table[i]
@@ -27,36 +35,63 @@ class BipedController:
             self.dic_vel_time[pair_table[i, 1]] = pair_table[i, 7]
         self.pair_table = pair_table
 
-    def control_para_calculation(self, des_vel, x_now):
+    # -----------------计算控制量-------------------
+    def control_para_calculation(self):
+        des_vel = self.this_rv
+        x_now = self.this_x
         vel_list = self.pair_table[:, 1]
         if des_vel > vel_list.max() or des_vel < vel_list.min():
             print("Error, input wrong velocity!")
-            
+        m_idx = np.fabs(vel_list - des_vel).argmin()
+        m_pair = self.pair_table[m_idx]
+        # 计算该状态下的delta控制量
+        delta_x = x_now - m_pair[0: 3]
+        jac = self.dic_vel_jac[m_pair[1]]
+        delta_u = np.dot(jac, delta_x.transpose())
+        delta_u_out = np.array((4,))
+        delta_u_out[0:3] = delta_u
+        delta_u_out[-1] += -delta_u[-1]      # 冗余量约束设计得到
+        self.this_pair = m_pair
+        self.this_du = delta_u_out
 
-    # 输出[tau1 ……tau5]的控制量
+    # -----------输出[tau1 ……tau5]的控制量-----------
     def robot_control(self):
+        # 获取机器人状态
+        vel = p.getBaseVelocity(self.robot_id)
+        pos = p.getBasePositionAndOrientation(self.robot_id)
         contact_list = p.getContactPoints(self.robot_id, planeId)
-        if len(contact_list) == 1:
-            body_pos = p.getBasePositionAndOrientation(self.robot_id)
+
+        if len(contact_list):
+            # body_pos = p.getBasePositionAndOrientation(self.robot_id)
             self.status = 'ground'
-        # if self.status == 'air':
+        else:
+            self.status = 'air'
+
+        if self.status == 'air':
+            if len(self.this_x) == 0:   # 如果没有初始高度数据
+                h0 = pos[0][2] + 0.5*vel[0][2]*vel[0][2]/g
+                self.this_x = [h0, vel[0][0], vel[0][1]]
+                self.control_para_calculation()         # 计算控制参数
+            # 计算当前腿部规划点
+
 
 
 
 # 准备环境
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0, 0, -10)
+p.setGravity(0, 0, -g)
 
 # 创建模型
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0, 0, 1.5]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 RobotId = p.loadURDF("bipedRobotOne.urdf", cubeStartPos, cubeStartOrientation)
-p.resetBaseVelocity(RobotId, [0, 0, 0])
+p.resetBaseVelocity(RobotId, [1, 0, 0])
 mode = p.VELOCITY_CONTROL
 # 控制器
 bc = BipedController(RobotId)
+bc.load_table('./data/stable_pair.csv')
 
 for i in range(600):
     # 控制程序
@@ -64,7 +99,7 @@ for i in range(600):
     if bc.status == 'ground':
         break
     p.stepSimulation()
-    time.sleep(1./100.0)
+    time.sleep(sim_cycle)
 
 p.disconnect()
 
